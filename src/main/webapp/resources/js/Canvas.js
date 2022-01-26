a7c4ba7c52954479780df829d4c83451581f25b5
alexmax100500 <alexmax10500@gmail.com>
2022-01-26 21:03:06 +0300

    updated Canvas.js

--- src/main/webapp/resources/js/Canvas.js
+++ src/main/webapp/resources/js/Canvas.js
@@ -10,16 +10,23 @@ firstQ = document.getElementById('1Q');
 secondQ = document.getElementById('2Q');
 thirdQ = document.getElementById('3Q');
 
-if(zeroQ!=null)
-ctx.drawImage(zeroQ,440,15,295,295);
-if(firstQ!=null)
-ctx.drawImage(firstQ,152,15,295,295);
-if(secondQ!=null)
-ctx.drawImage(secondQ,152,305,295,295);
-if(thirdQ!=null)
-ctx.drawImage(thirdQ,448,305,295,295);
+Pallet.onload = function() {
+    ctx.drawImage(Pallet, 150, 0, 600, 600);
+}
 
-ctx.drawImage(Pallet,150,0,600,600);
+zeroQ.onload = function () {
+    ctx.drawImage(zeroQ, 440, 15, 295, 295);
+}
+firstQ.onload = function() {
+    ctx.drawImage(firstQ, 152, 15, 295, 295);
+}
+secondQ.onload = function() {
+    ctx.drawImage(secondQ, 152, 305, 295, 295);
+}
+thirdQ.onload = function() {
+    ctx.drawImage(thirdQ, 448, 305, 295, 295);
+}
+
 
 function loadGraph()
 {
--- src/main/webapp/resources/js/UpdateValsOnChange.js
+++ src/main/webapp/resources/js/UpdateValsOnChange.js
@@ -1,9 +1,9 @@
-$('select').on('change', function() {
-    alert( this.value ); // or $(this).val()
-});
-function getval(sel)
-{
-    if(sel.value=="triangle")
-        document.getElementsByName("triangle").style.display = "none";
-    //alert(sel.value);
-}
+// $('select').on('change', function() {
+//     alert( this.value ); // or $(this).val()
+// });
+// function getval(sel)
+// {
+//     if(sel.value=="triangle")
+//         document.getElementsByName("triangle").style.display = "none";
+//     //alert(sel.value);
+// }
