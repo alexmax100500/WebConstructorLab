dbbaf01bbdf0a0ded04cc3458c36301d9ee6e536
Latone <zhuchkov_01@mail.ru>
2022-01-26 19:57:01 +0300

    -Visualized graph

---
+++ src/main/webapp/resources/sql/figurePathsDML.sql
@@ -0,0 +1,50 @@
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/0el1q.png','triangle',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/1el1q.png','triangle',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/2el1q.png','triangle',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/3el1q.png','triangle',1);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/0el2q.png','triangle',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/1el2q.png','triangle',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/2el2q.png','triangle',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/3el2q.png','triangle',2);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/0el3q.png','triangle',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/1el3q.png','triangle',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/2el3q.png','triangle',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/3el3q.png','triangle',3);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/0el4q.png','triangle',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/1el4q.png','triangle',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/2el4q.png','triangle',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/triangle/3el4q.png','triangle',4);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/0.png','square',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/1el1q.png','square',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/2el1q.png','square',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/3el1q.png','square',1);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/0.png','square',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/1el2q.png','square',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/2el2q.png','square',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/3el2q.png','square',2);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/0.png','square',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/1el3q.png','square',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/2el3q.png','square',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/3el3q.png','square',3);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/0.png','square',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/1el4q.png','square',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/2el4q.png','square',4);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/square/3el4q.png','square',4);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/0el1q.png','circle',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/0el2q.png','circle',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/0el3q.png','circle',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/0el4q.png','circle',4);
+
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/1el1q.png','circle',1);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/1el2q.png','circle',2);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/1el3q.png','circle',3);
+INSERT INTO t_figure(path,type,quarter) values ('resources/static/circle/1el4q.png','circle',4);
+
