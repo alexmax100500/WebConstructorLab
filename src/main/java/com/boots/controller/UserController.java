a3c0bce8e681de83066a41ad5faf30ad30aa0a79
alexmax100500 <alexmax10500@gmail.com>
2022-01-28 16:08:00 +0300

    updated UserController

--- src/main/java/com/boots/controller/UserController.java
+++ src/main/java/com/boots/controller/UserController.java
@@ -2,11 +2,13 @@ package com.boots.controller;
 
 import com.boots.entity.Graph.ComposedFigure;
 import com.boots.entity.GraphStructure;
+import com.boots.service.shapes.Checker;
 import com.boots.service.shapes.GraphShapes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -14,15 +16,25 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class UserController {
     @Autowired
-    GraphShapes graphShapes;
-    @Autowired
     GraphStructure<ComposedFigure> submittedStructure;
+    @Autowired
+    Checker checker;
     @GetMapping("/userconstruct")
      public String userconstruct(Model model,
                                  HttpServletRequest request,
                                  HttpSession session){
         model.addAttribute("submittedStructure", submittedStructure);
         return "/userconstruct";
+    }
+    @PostMapping("/dots")
+        public String dots(Model model,
+                           HttpServletRequest request,
+                           HttpSession session ){
+        int x = (int) request.getAttribute("X");
+        int y = (int) request.getAttribute("Y");
+        int r = (int) request.getAttribute("R");
+        boolean t = checker.check(x,y,r);
+        return "X: "+x+" Y: "+y+" R: "+r+ " T: "+ t;
     }
 
 }
